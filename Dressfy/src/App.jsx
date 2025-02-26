6bf319ee63150a112ba993c10b95bef5c04c8e2b
nithula20020206 <nithula.20231213@iit.ac.lk>
2025-02-26 12:48:55 +0530

    addd something

---
+++ Dressfy/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css"
+import CreateAccount from "./pages/signup_page"
+import Login from "./pages/login"
+
+function App() {
+  return (
+    <div className="app-container">
+      <CreateAccount/>
+    </div>
+  )
+}
+
+export default App
+
