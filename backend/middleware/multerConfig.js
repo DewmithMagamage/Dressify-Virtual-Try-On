ad39dd432c9b4185c7a9017ca0307386caa6d98a
chethmiYWD <chethmi.20230809@iit.ac.lk>
2025-03-13 20:15:09 +0530

    Updated multerConfig.js

--- backend/middleware/multerConfig.js
+++ backend/middleware/multerConfig.js
@@ -1,17 +1,17 @@
-// import multer from 'multer';
-// import path from 'path';
-// import { CONFIG } from '../config/config.js';
+import multer from 'multer';
+import path from 'path';
+import { CONFIG } from '../config/config.js';
 
-// // Configure multer
-// const storage = multer.diskStorage({
-//   destination: "uploads/",
-//   filename: (req, file, cb) => {
-//     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-//     cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
-//   }
-// });
+// Configure multer
+const storage = multer.diskStorage({
+  destination: "uploads/",
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
+  }
+});
 
-// export const upload = multer({
-//   storage: storage,
-//   limits: { fileSize: CONFIG.MAX_FILE_SIZE }
-// });
+export const upload = multer({
+  storage: storage,
+  limits: { fileSize: CONFIG.MAX_FILE_SIZE }
+});
