6ee898097eb9b898a522819e69a0e59d31a9b28a
chethmiYWD <chethmi.20230809@iit.ac.lk>
2025-03-13 20:17:12 +0530

    Updated systemUtils.js

--- backend/utils/systemUtils.js
+++ backend/utils/systemUtils.js
@@ -1,21 +1,21 @@
-// import os from 'os';
+import os from 'os';
 
-// // Debugging utility functions
-// export const logSystemInfo = () => {
-//   console.log('Current working directory:', process.cwd());
-//   console.log('Temp directory:', os.tmpdir());
-//   console.log('Platform:', process.platform);
-// };
+// Debugging utility functions
+export const logSystemInfo = () => {
+  console.log('Current working directory:', process.cwd());
+  console.log('Temp directory:', os.tmpdir());
+  console.log('Platform:', process.platform);
+};
 
-// export const setupErrorHandlers = () => {
-//   // Error handling
-//   process.on('uncaughtException', (error) => {
-//     console.error('Uncaught Exception:', error);
-//     if (process.env.NODE_ENV !== 'production') process.exit(1);
-//   });
+export const setupErrorHandlers = () => {
+  // Error handling
+  process.on('uncaughtException', (error) => {
+    console.error('Uncaught Exception:', error);
+    if (process.env.NODE_ENV !== 'production') process.exit(1);
+  });
 
-//   process.on('unhandledRejection', (reason, promise) => {
-//     console.error('Unhandled Rejection at:', promise, 'reason:', reason);
-//     if (process.env.NODE_ENV !== 'production') process.exit(1);
-//   });
-// };
+  process.on('unhandledRejection', (reason, promise) => {
+    console.error('Unhandled Rejection at:', promise, 'reason:', reason);
+    if (process.env.NODE_ENV !== 'production') process.exit(1);
+  });
+};
